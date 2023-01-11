a10ac104a792393430620850b49d00334a5fef89
cleber2099 <cleber.santos.2099@gmail.com>
2023-01-11 02:47:49 -0300

    exceptions - provocando uma

--- lib/13_exceptions/exceptions.dart
+++ lib/13_exceptions/exceptions.dart
@@ -1,3 +1,20 @@
 void main(){
-  
+  var idade =" 23 anos";
+  String? nome;
+  try{
+    int.parse(idade);
+    //nome!.toLowerCase();
+    if(nome == null){
+      throw new FormatException("nome não pode ser nulo");
+    }
+
+   }on FormatException catch(e, s){
+    // print("erro ao converter idade ");
+   }
+  catch(erro){
+    print("erro ao converter nome");
+  } finally{
+    print("fim do programa");
+  }
+
 }
